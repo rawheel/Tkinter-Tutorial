07d8c54351bc78d4c698f7dd1e8bc3fb60b3a70a
Raheel Siddiqui <rawheeelalisiddiqui@gmail.com>
2020-05-28 14:42:20 +0500

    episode 06

---
+++ RadioButton Ep 06.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title("PYTROOPS")
+window.configure(background = "yellow")
+window.geometry('200x200')
+
+
+pytroop = StringVar()
+pytroop.set("Blue")
+
+
+
+def print(ans):
+    Label(window , text = ans).pack()
+
+Radiobutton(window , text = "Blue" , variable = pytroop , value = "Blue" ).pack()
+Radiobutton(window , text = "Green" , variable = pytroop , value = "Green" ).pack()
+Radiobutton(window , text = "Red" , variable = pytroop , value = "Red" ).pack()
+
+a = Button(window , text = "select" , command = lambda : print(pytroop.get())).pack()
+
+window.mainloop()
